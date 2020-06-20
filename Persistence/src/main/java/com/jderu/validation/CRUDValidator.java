af9eaf7836f19cb75cee5771d2da273e4138bf71
Razvan-Emanuel Surubariu <jdery99@gmail.com>
2020-06-20 23:51:07 +0300

    Solved repo

--- Persistence/src/main/java/com/jderu/validation/CRUDValidator.java
+++
@@ -1,9 +0,0 @@
-package com.jderu.validation;
-
-public interface CRUDValidator<E> {
-    /**
-     * @param entity the entity to be validated
-     * @throws ValidationException if the entity doesnt meet the specified characteristics
-     */
-    void validate(E entity) throws ValidationException;
-}
--- Persistence/src/main/java/com/jderu/validation/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.jderu.validation;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
