6cf37b3bb42fa950065da6102401546badaa8b1c
Razvan-Emanuel Surubariu <jdery99@gmail.com>
2020-06-21 13:56:25 +0300

    added rest controller

--- Domain/src/main/java/com/jderu/domain/EmployeeState.java
+++
@@ -1,6 +0,0 @@
-package com.jderu.domain;
-
-public enum EmployeeState {
-    ATWORK,
-    ATHOME
-}
--- Domain/src/main/java/com/jderu/domain/Entity.java
+++
@@ -1,9 +0,0 @@
-package com.jderu.domain;
-
-import java.io.Serializable;
-
-public interface Entity<ID extends Serializable> extends Serializable {
-    ID getId();
-
-    void setId(ID id);
-}
